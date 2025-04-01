08902dd65eea26a9af40cbd5dd956026453a67f5
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-04-01 22:32:19 +0530

    fix: update CORS configuration to allow requests from the new frontend URL

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ if (cluster.isMaster) {
   app.use(express.urlencoded({ extended: true }));
 
   app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   }));
   
